6022b1e1a4ea81c22747be18d5456ba2187e6541
adi3120 <aditya.yadav.cs.20@ggits.net>
2022-04-15 08:57:13 +0530

    added footer

--- m_r_s.py
+++ m_r_s.py
@@ -93,3 +93,19 @@ if genre_recommend:
 	if recs is not None:
 		st.dataframe(recs)
 
+footer="""<style>
+.footer {
+position:fixed;
+left: 0;
+bottom: 0;
+width: 100%;
+background-color: black;
+color: white;
+text-align: center;
+}
+</style>
+<div class="footer">
+<p>Developed by Aditya Yadav & Ananya Karne</p>
+</div>
+"""
+st.markdown(footer,unsafe_allow_html=True)
